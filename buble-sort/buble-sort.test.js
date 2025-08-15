24cff89acb646358f10a13447f55aa37aa7040ee
Rezi Karanadze <karanadzerevazi@gmail.com>
2025-08-15 22:24:45 +0400

    implement buble sort challange

---
+++ buble-sort/buble-sort.test.js
@@ -0,0 +1,7 @@
+function bubleSort(num) {}
+
+test("buble-sort", function () {
+  const nums = [10, 5, 3, 8, 2, 6, 4, 7, 9, 1];
+  const sortedArray = bubleSort(nums);
+  expect(sortedArray).toEqual([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
+});
