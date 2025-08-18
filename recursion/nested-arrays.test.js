d4a1f5ad864da00ea7e54c7e6c3ac9c20b6fd7f0
Rezi Karanadze <karanadzerevazi@gmail.com>
2025-08-18 21:51:31 +0400

    implement nested array solution and factorial using recursion

--- recursion/nested-arrays.test.js
+++ recursion/nested-arrays.test.js
@@ -1,4 +1,17 @@
-function nestedAdd(array) {}
+function nestedAdd(array) {
+  let sum = 0;
+  for (let i = 0; i < array.length; i++) {
+    const current = array[i];
+
+    if (Array.isArray(current)) {
+      sum += nestedAdd(current);
+    } else {
+      sum += current;
+    }
+  }
+
+  return sum;
+}
 
 test("nested arrays addition", () => {
   expect(nestedAdd([1, 2, [3, 4, [5, 6]]])).toBe(21);
