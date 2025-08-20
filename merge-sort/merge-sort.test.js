1c960562a9ffc204b040f2aa3add5d2921b8a536
Rezi Karanadze <karanadzerevazi@gmail.com>
2025-08-20 21:54:46 +0400

    add hints for challnage

--- merge-sort/merge-sort.test.js
+++ merge-sort/merge-sort.test.js
@@ -1,4 +1,10 @@
-function mergeSort() {}
+function mergeSort(nums) {
+  // base case
+  // we need to slipt our array by 2
+  // our recursion of two sorted lists and return sorted list
+}
+
+// helper function for make merge of this two array: left | right
 
 test("merge sort", function () {
   const nums = [10, 5, 3, 8, 2, 6, 4, 7, 9, 1];
