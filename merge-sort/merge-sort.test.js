51eac88c34c46dfbc2582a1a6483c36a90e3d9d6
Rezi Karanadze <karanadzerevazi@gmail.com>
2025-08-20 21:52:32 +0400

    implement challange merge sort

---
+++ merge-sort/merge-sort.test.js
@@ -0,0 +1,7 @@
+function mergeSort() {}
+
+test("merge sort", function () {
+  const nums = [10, 5, 3, 8, 2, 6, 4, 7, 9, 1];
+  const sorted = mergeSort(nums);
+  expect(sorted).toEqual([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
+});
