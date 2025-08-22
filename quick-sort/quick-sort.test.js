277a814318497fc7b57e23d92dd78a16ab2860b6
Rezi Karanadze <karanadzerevazi@gmail.com>
2025-08-22 21:41:42 +0400

    implement quick sort solution

--- quick-sort/quick-sort.test.js
+++ quick-sort/quick-sort.test.js
@@ -1,8 +1,23 @@
 function quickSort(nums) {
   // base case if array length 0, 1
+  if (nums.length <= 1) return nums;
   // get pivot as last number
+  const pivot = nums[nums.length - 1];
+  const left = [];
+  const right = [];
   // smaller array sort, then larger array sort based on pivot
+  for (let i = 0; i < nums.length - 1; i++) {
+    if (nums[i] < pivot) {
+      left.push(nums[i]);
+    } else {
+      right.push(nums[i]);
+    }
+  }
+
+  const sortedLeft = quickSort(left);
+  const sortedRight = quickSort(right);
   // call quick sort with recursion left and right and contact and return
+  return sortedLeft.concat(pivot, sortedRight);
 }
 
 test("quickSOrt", function () {
