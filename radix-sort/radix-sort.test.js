e780ec27c408142a4fe93b4481cdbfab5e4cedd1
Rezi Karanadze <karanadzerevazi@gmail.com>
2025-08-22 21:55:08 +0400

    implement test for radix sort

--- radix-sort/radix-sort.test.js
+++ radix-sort/radix-sort.test.js
@@ -4,3 +4,25 @@ function radixSort(array) {
   // create helper to get digits
   // put digit by digit in bucket and after move out
 }
+
+describe("radix sort", function () {
+  it("should sort correctly", () => {
+    const nums = [
+      20, 51, 3, 801, 415, 62, 4, 17, 19, 11, 1, 100, 1244, 104, 944, 854, 34,
+      3000, 3001, 1200, 633,
+    ];
+    const ans = radixSort(nums);
+    expect(ans).toEqual([
+      1, 3, 4, 11, 17, 19, 20, 34, 51, 62, 100, 104, 415, 633, 801, 854, 944,
+      1200, 1244, 3000, 3001,
+    ]);
+  });
+  it("should sort 99 random numbers correctly", () => {
+    const fill = 99;
+    const nums = new Array(fill)
+      .fill()
+      .map(() => Math.floor(Math.random() * 500000));
+    const ans = radixSort(nums);
+    expect(ans).toEqual(nums.sort());
+  });
+});
