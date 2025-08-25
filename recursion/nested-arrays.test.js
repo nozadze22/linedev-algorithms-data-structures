9f80f72e195e6b8dd58d7cdf32646b7768c3ac6e
giorgi <127527335+nozadze22@users.noreply.github.com>
2025-08-25 21:44:34 +0400

    new changes

--- recursion/nested-arrays.test.js
+++ recursion/nested-arrays.test.js
@@ -1,4 +1,16 @@
-function nestedAdd(array) {}
+function nestedAdd(array) {
+  let sum = 0;
+
+  for (const item of array) {
+    if (Array.isArray(item)) {
+      sum += nestedAdd(item); // რეკურსიულად დაითვლის შიდა მასივის ჯამს
+    } else {
+      sum += item;
+    }
+  }
+
+  return sum;
+}
 
 test("nested arrays addition", () => {
   expect(nestedAdd([1, 2, [3, 4, [5, 6]]])).toBe(21);
